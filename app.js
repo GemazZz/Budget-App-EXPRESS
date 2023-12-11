676330db2ebf970849083cdc3161d3f7af73d4d8
Your Name <gogemaza14@gmail.comm>
2023-12-11 18:16:58 +0400

    Full authorization

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+require("dotenv").config();
 const app = express();
 app.use(express.json());
 require("./db");
+const authorization = require("./middlewares");
 
 const cors = require("cors");
 const corsOpt = {
   origin: "http://localhost:3001",
 };
 app.use(cors(corsOpt));
-
-const SECRET_KEY = "hello";
 
 const User = require("./models/users.models");
 
@@ -40,30 +40,38 @@ app.post("/api/v1/signup", async (req, res) => {
 app.post("/api/v1/signin", async (req, res) => {
   const { email, password } = req.body;
   const foundUser = await User.findOne({ email: email, status: "active" });
+  if (foundUser === null) {
+    return res.status(400).json("User does not exist!");
+  }
+  const userId = foundUser._id;
   if (foundUser && (await bcrypt.compare(password, foundUser.password))) {
-    const token = jwt.sign({ userId: foundUser._id, email: foundUser.email }, SECRET_KEY, { expiresIn: "5h" });
-    return res.json(token);
+    const token = jwt.sign({ userId }, process.env.secretKey, { expiresIn: "5h" });
+    return res.json({ token });
   }
   return res.status(400).json("Wrong credentials");
 });
 
-app.post("/api/v1/newpassword", async (req, res) => {
-  const { email, password, newPassword } = req.body;
+app.post("/api/v1/newpassword", authorization, async (req, res) => {
+  const { userId } = req.user;
+  const { password, newPassword } = req.body;
   const hashedNewPassword = await bcrypt.hash(newPassword, 10);
-  const foundUser = await User.findOne({ email: email, status: "active" });
-  const isPasswordCorrect = await bcrypt.compare(password, foundUser.password);
-  if (!isPasswordCorrect) {
-    return res.status(400).json({ error: "Wrong password" });
+  const foundUser = await User.findOne({ _id: userId, status: "active" });
+  if (foundUser && (await bcrypt.compare(password, foundUser.password))) {
+    await foundUser.updateOne({ password: hashedNewPassword });
+    return res.status(200).json({ message: "Password changed successfully!" });
   }
-  await foundUser.updateOne({ password: hashedNewPassword });
-  res.status(200).json({ message: "Password changed successfully!" });
+  return res.status(400).json({ error: "Wrong password" });
 });
 
-app.post("/api/v1/deactivation", async (req, res) => {
-  const { email, password } = req.body;
-  const foundUser = await User.findOne({ email: email, status: "active" });
-  await foundUser.updateOne({ status: "deactivated", deactivated_at: new Date() });
-  res.status(200).json("Password changed successfully!");
+app.post("/api/v1/deactivation", authorization, async (req, res) => {
+  const { password } = req.body;
+  const { userId } = req.user;
+  const foundUser = await User.findOne({ _id: userId, status: "active" });
+  if (foundUser && (await bcrypt.compare(password, foundUser.password))) {
+    await foundUser.updateOne({ status: "deactivated", deactivated_at: new Date() });
+    return res.status(200).json("account deactivated successfully!");
+  }
+  return res.status(400).json("");
 });
 
 app.listen(3000, () => {
